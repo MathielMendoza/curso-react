5791c2161cfa4ae7369536d0283a562f0b52864c
Gerson Mendoza <gerbo0906@gmail.com>
2023-01-20 20:14:48 -0600

    Hooks: added useEffect into Message component

--- src/Message.js
+++ src/Message.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 export const Message = () => {
     const [message, setMessage] = useState('');
@@ -6,6 +6,14 @@ export const Message = () => {
     const handleChange = (e) => setMessage(e.target.value);
 
     const handleClick = () => alert('Hello: ' + message);
+
+    useEffect(() => {
+        alert('Disparado desde useEffect: ' + message);
+    }, [message]);
+
+    //Todo: sin []     => Se ejecutara cuando se genere un cambio en el componente(global)
+    //Todo: []         => Solo se ejecutara 1 vez
+    //Todo: [dependencia] => Cuando se genere un cambio en la variable
 
 
     return (
