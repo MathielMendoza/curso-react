909bc1aca3bc19c18a2d0e2f8db8381bcfb91e4f
Gerson-Mendoza <gerbo0906@gmail.com>
2023-01-24 15:18:43 -0600

    vite: updated app style

--- task-app/src/App.jsx
+++ task-app/src/App.jsx
@@ -3,9 +3,8 @@ import TaskList from "./components/TaskList.jsx";
 
 const App = () => {
 	return (
-		<main className='bg-zinc-900 h-screen h-full py-3'>
+		<main className='bg-zinc-900 h-screen py-4'>
 			<div className='container mx-auto'>
-				<h1>Task App</h1>
 				<TaskForm/>
 				<TaskList/>
 			</div>
